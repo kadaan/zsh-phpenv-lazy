0dc795fd6fe52686829e1c6e09d08a32dc853166
Joel Baranick <joel.baranick@ensighten.com>
2018-12-01 01:43:09 -0800

    Fix lookup

--- phpenv-lazy.plugin.zsh
+++ phpenv-lazy.plugin.zsh
@@ -2,7 +2,7 @@
 if type phpenv &> /dev/null; then
   local PHPENV_SHIMS="${PHPENV_ROOT:-${HOME}/.phpenv}/shims"
   export PATH="${PHPENV_SHIMS}:${PATH}"
-  source $(which phpenv)/../../completions/phpenv.zsh
+  source $(whence -p phpenv)/../../completions/phpenv.zsh
   function phpenv() {
     unset -f phpenv > /dev/null 2>&1
     eval "$(command phpenv init -)"
