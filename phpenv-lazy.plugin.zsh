43c77de1279416c35734004e9917441f15cb2047
Joel Baranick <joel.baranick@ensighten.com>
2019-03-11 16:23:56 -0700

    Fix completions location, now requires greadlink

--- phpenv-lazy.plugin.zsh
+++ phpenv-lazy.plugin.zsh
@@ -2,7 +2,7 @@
 if type phpenv &> /dev/null; then
   local PHPENV_SHIMS="${PHPENV_ROOT:-${HOME}/.phpenv}/shims"
   export PATH="${PHPENV_SHIMS}:${PATH}"
-  source $(whence -p phpenv)/../../completions/phpenv.zsh
+  source $(dirname $(greadlink `whence -p phpenv`))/../completions/phpenv.zsh
   function phpenv() {
     unset -f phpenv > /dev/null 2>&1
     eval "$(command phpenv init -)"
